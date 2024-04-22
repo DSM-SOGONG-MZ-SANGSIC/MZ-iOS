fff04ed1ef96313d13c874763768195faa353a24
Byung jin <bjcho1503@naver.com>
2024-04-22 14:49:44 +0900

    ⚙️ :: 폰트 디자인시스템 추가

---
+++ App/Sources/Extension/Font+.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+extension UIFont {
+    // MARK: - Header
+    static let headerH1ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 30)
+    static let headerH1Bold = MZAPPFontFamily.Pretendard.bold.font(size: 30)
+    static let headerH1SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 30)
+    static let headerH1Medium = MZAPPFontFamily.Pretendard.medium.font(size: 30)
+    static let headerH1Light = MZAPPFontFamily.Pretendard.light.font(size: 30)
+    static let headerH1Thin = MZAPPFontFamily.Pretendard.thin.font(size: 30)
+
+    static let headerH2ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 24)
+    static let headerH2Bold = MZAPPFontFamily.Pretendard.bold.font(size: 24)
+    static let headerH2SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 24)
+    static let headerH2Medium = MZAPPFontFamily.Pretendard.medium.font(size: 24)
+    static let headerH2Light = MZAPPFontFamily.Pretendard.light.font(size: 24)
+    static let headerH2Thin = MZAPPFontFamily.Pretendard.thin.font(size: 24)
+
+    static let headerH3ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 20)
+    static let headerH3Bold = MZAPPFontFamily.Pretendard.bold.font(size: 20)
+    static let headerH3SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 20)
+    static let headerH3Medium = MZAPPFontFamily.Pretendard.medium.font(size: 20)
+    static let headerH3Light = MZAPPFontFamily.Pretendard.light.font(size: 20)
+    static let headerH3Thin = MZAPPFontFamily.Pretendard.thin.font(size: 20)
+
+    // MARK: - Body
+    static let bodyB1ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 16)
+    static let bodyB1Bold = MZAPPFontFamily.Pretendard.bold.font(size: 16)
+    static let bodyB1SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 16)
+    static let bodyB1Medium = MZAPPFontFamily.Pretendard.medium.font(size: 16)
+    static let bodyB1Light = MZAPPFontFamily.Pretendard.light.font(size: 16)
+    static let bodyB1Thin = MZAPPFontFamily.Pretendard.thin.font(size: 16)
+    
+    static let bodyB2ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 14)
+    static let bodyB2Bold = MZAPPFontFamily.Pretendard.bold.font(size: 14)
+    static let bodyB2SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 14)
+    static let bodyB2Medium = MZAPPFontFamily.Pretendard.medium.font(size: 14)
+    static let bodyB2Light = MZAPPFontFamily.Pretendard.light.font(size: 14)
+    static let bodyB2Thin = MZAPPFontFamily.Pretendard.thin.font(size: 14)
+    
+    static let bodyB3ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 13)
+    static let bodyB3Bold = MZAPPFontFamily.Pretendard.bold.font(size: 13)
+    static let bodyB3SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 13)
+    static let bodyB3Medium = MZAPPFontFamily.Pretendard.medium.font(size: 13)
+    static let bodyB3Light = MZAPPFontFamily.Pretendard.light.font(size: 13)
+    static let bodyB3Thin = MZAPPFontFamily.Pretendard.thin.font(size: 13)
+
+    // MARK: - Caption
+
+    static let captionC1ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 12)
+    static let captionC1Bold = MZAPPFontFamily.Pretendard.bold.font(size: 12)
+    static let captionC1SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 12)
+    static let captionC1Medium = MZAPPFontFamily.Pretendard.medium.font(size: 12)
+    static let captionC1Light = MZAPPFontFamily.Pretendard.light.font(size: 12)
+    static let captionC1Thin = MZAPPFontFamily.Pretendard.thin.font(size: 12)
+
+    static let captionC2ExtraBold = MZAPPFontFamily.Pretendard.extraBold.font(size: 10)
+    static let captionC2Bold = MZAPPFontFamily.Pretendard.bold.font(size: 10)
+    static let captionC2SemiBold = MZAPPFontFamily.Pretendard.semiBold.font(size: 10)
+    static let captionC2Medium = MZAPPFontFamily.Pretendard.medium.font(size: 10)
+    static let captionC2Light = MZAPPFontFamily.Pretendard.light.font(size: 10)
+    static let captionC2Thin = MZAPPFontFamily.Pretendard.thin.font(size: 10)
+}
