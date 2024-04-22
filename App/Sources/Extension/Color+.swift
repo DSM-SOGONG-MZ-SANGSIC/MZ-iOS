0cc061adf3eea58ce110fe95f525cddac1a3f1b1
Byung jin <bjcho1503@naver.com>
2024-04-22 15:07:01 +0900

    ⚙️ :: Color 디자인 시스템 추가

---
+++ App/Sources/Extension/Color+.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UIColor {
+    // MARK: - Gray
+    static let gray100 = MZAPPAsset.gray100.color
+    static let gray200 = MZAPPAsset.gray200.color
+    static let gray300 = MZAPPAsset.gray300.color
+    static let gray400 = MZAPPAsset.gray400.color
+    static let gray50 = MZAPPAsset.gray50.color
+    static let gray500 = MZAPPAsset.gray500.color
+    static let gray600 = MZAPPAsset.gray600.color
+    static let gray700 = MZAPPAsset.gray700.color
+    static let gray800 = MZAPPAsset.gray800.color
+    static let gray900 = MZAPPAsset.gray900.color
+}
+
